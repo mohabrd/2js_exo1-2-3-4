964b4d5c25a6432fa61019acbaa82c8ec2d729e7
mohabrd <bezrhoudimohamed@gmail.com>
2022-12-20 11:53:33 +0100

    exo4

---
+++ js_exo4/public/js/main.js
@@ -0,0 +1,13 @@
+let quatriemearay = ['coding 20','coding 21','coding 22'];
+console.log(quatriemearay);
+
+quatriemearay.push("Mohamed");
+console.log(quatriemearay);
+
+quatriemearay.splice(3, 1, 'coding moha');
+
+console.log(quatriemearay);
+
+quatriemearay.pop();
+
+console.log(quatriemearay);
