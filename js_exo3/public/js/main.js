c8cab0350ea57081da5422c76af0c0bb17433795
mohabrd <bezrhoudimohamed@gmail.com>
2022-12-20 11:49:32 +0100

    exo3

---
+++ js_exo3/public/js/main.js
@@ -0,0 +1,9 @@
+let troisiemeearay = ['coding 20','coding 21','coding 22'];
+console.log(troisiemeearay);
+
+troisiemeearay.push("Mohamed");
+console.log(troisiemeearay);
+
+troisiemeearay.splice(3, 1, 'coding moha');
+
+console.log(troisiemeearay);
